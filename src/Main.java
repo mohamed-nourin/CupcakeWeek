71bf1c9525038ff16b06d95d8f368b6c9b1aff25
Mohamed Nourin <mohamed0nourin@gmail.com>
2023-08-30 11:42:55 -0600

    unit2 revised

--- src/Main.java
+++ src/Main.java
@@ -1,83 +1,115 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, IOException {
         ArrayList<Cupcake> cupcakeMenu = new ArrayList<Cupcake>();
-        ArrayList<Drink> drinkMenu = new ArrayList<Drink>();
         Cupcake cupcake = new Cupcake();
         RedVelvet redVelvet = new RedVelvet();
         Chocolate chocolate = new Chocolate();
-        Drink water = new Drink();
-        Soda soda = new Soda();
-        Milk milk = new Milk();
-
         System.out.println("We are in the middle of creating the cupcake menu. We currently have three cupcakes on the menu but we need to decide on pricing.");
-
         Scanner input = new Scanner(System.in);
-
         System.out.println("We are deciding on the price for our standard cupcake. Here is the description:");
         cupcake.type();
-
-        System.out.println("How much would you like to charge for the cupcake? (Enter a numerical number taken to 2 decimal places)");
-
+        System.out.println("How much would you like to charge for the cupcake? (Input a numerical number taken to 2 decimal places)");
         String priceText = input.nextLine();
         double price = Double.parseDouble(priceText);
         cupcake.setPrice(price);
-
-        System.out.println("We are deciding on the price for our red velvet cupcake. Here is the description: ");
+        System.out.println("We are deciding on the price for our red velvet cupcake. Here is the description:");
         redVelvet.type();
-        System.out.println("How much would you like to charge for the cupcake? (Input a numerical number taken to 2 decimal places)");
-
-        String priceTextRedVelvet = input.nextLine();
-        double priceRedVelvet = Double.parseDouble((priceTextRedVelvet));
-
-        redVelvet.setPrice(priceRedVelvet);
-
-        System.out.println("We are deciding the price for our chocolate cupcake. Here is the description: ");
+        System.out.println("How much would you like to charge for the red velvet cupcake? (Input a numerical number taken to 2 decimal places)");
+        String redVelvetPriceText = input.nextLine();
+        double redVelvetPrice = Double.parseDouble(redVelvetPriceText);
+        redVelvet.setPrice(redVelvetPrice);
+        System.out.println("We are deciding on the price for our chocolate cupcake. Here is the description:");
         chocolate.type();
-
-        System.out.println("How much would you like to charge for the cupcake? (Input a numerical number taken to 2 decimal places)");
-
-        String priceTextChocolate = input.nextLine();
-        double priceChocolate = Double.parseDouble(priceTextChocolate);
-
-        chocolate.setPrice(priceChocolate);
-
-        System.out.println("We are deciding the price for water. Here is the description: ");
-        water.type();
-        System.out.println("How much would you like to charge for water? (Input a numerical number taken to 2 decimal places)");
-
-        String priceTextWater = input.nextLine();
-        double priceWater = Double.parseDouble(priceTextWater);
-
-        water.setPrice(priceWater);
-
-        System.out.println("We are deciding the price for soda. Here is the description: ");
-        soda.type();
-        System.out.println("How much would you like to charge for soda? (Input a numerical number taken to 2 decimal places)");
-
-        String priceTextSoda = input.nextLine();
-        double priceSoda = Double.parseDouble(priceTextSoda);
-
-        soda.setPrice(priceSoda);
-
-        System.out.println("We are deciding the price for milk. Here is the description: ");
-        milk.type();
-        System.out.println("How much would you like to charge for milk? (Input a numerical number taken to 2 decimal places)");
-
-        String priceTextMilk = input.nextLine();
-        double priceMilk = Double.parseDouble(priceTextMilk);
-
-        milk.setPrice(priceMilk);
-
-
+        System.out.println("How much would you like to charge for the chocolate cupcake? (Input a numerical number taken to 2 decimal places)");
+        String chocolatePriceText = input.nextLine();
+        double chocolatePrice = Double.parseDouble(chocolatePriceText);
+        chocolate.setPrice(chocolatePrice);
         cupcakeMenu.add(cupcake);
         cupcakeMenu.add(redVelvet);
         cupcakeMenu.add(chocolate);
-        drinkMenu.add(water);
+        ArrayList<Drink> drinkMenu = new ArrayList<Drink>();
+        Drink drink = new Drink();
+        Soda soda = new Soda();
+        Milk milk = new Milk();
+        System.out.println("We are in the middle of creating the drink menu. We currently have three drinks on the menu but we need to decide on pricing.");
+        System.out.println("We are deciding on the price for our standard drink of water. Here is the description:");
+        drink.type();
+        System.out.println("How much would you like to charge for the drink? (Input a numerical number taken to 2 decimal places)");
+        priceText = input.nextLine();
+        price = Double.parseDouble(priceText);
+        drink.setPrice(price);
+        System.out.println("We are deciding on the price for our soda. Here is the description:");
+        soda.type();
+        System.out.println("How much would you like to charge for the soda? (Input a numerical number taken to 2 decimal places)");
+        priceText = input.nextLine();
+        price = Double.parseDouble(priceText);
+        soda.setPrice(price);
+        System.out.println("We are deciding on the price for our milk. Here is the description:");
+        milk.type();
+        System.out.println("How much would you like to charge for the milk? (Input a numerical number taken to 2 decimal places)");
+        priceText = input.nextLine();
+        price = Double.parseDouble(priceText);
+        milk.setPrice(price);
+        drinkMenu.add(drink);
         drinkMenu.add(soda);
         drinkMenu.add(milk);
         new Order(cupcakeMenu, drinkMenu);
+    }
+}
+class Cupcake{
+    public double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void type(){
+        System.out.println("A basic, generic cupcake, with vanilla frosting");
+    }
+
+}
+class RedVelvet extends Cupcake {
+    public void type() {
+        System.out.println("A red velvet based cupcake, with cream cheese frosting");
+
+    }
+}
+
+class Chocolate extends Cupcake {
+    public void type() {
+        System.out.println("A chocolate based cupcake, with chocolate frosting");
+    }
+}
+class Drink{
+    public double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void type() {
+        System.out.println("A bottle of water");
+    }
+}
+
+class Soda extends Drink{
+    public void type() {
+        System.out.println("A bottle of soda");
+    }
+}
+class Milk extends Drink{
+    public void type() {
+        System.out.println("A bottle of milk");
     }
 }
--- src/Order.java
+++ src/Order.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -5,148 +6,146 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.File;
-
-public class Order{
-    public Order(ArrayList<Cupcake> cupcakeMenu, ArrayList<Drink> drinkMenu) {
-
-        System.out.println("Hello customer. Would you like to place an order? (Y or N)");
+public class Order {
+    public Order(ArrayList<Cupcake> cupcakeMenu, ArrayList<Drink> drinkMenu) throws InterruptedException, IOException {
+        System.out.println("Hello Customer. Would you like to place an order? (Y/N)");
         Scanner input = new Scanner(System.in);
         String placeOrder = input.nextLine();
-
         ArrayList<Object> order = new ArrayList<Object>();
-
-        if (placeOrder.equalsIgnoreCase("Y")) {
+        if (placeOrder.equalsIgnoreCase("Y")){
             order.add(LocalDate.now());
             order.add(LocalTime.now());
-
-            System.out.println("Here is the menu");
-
-            System.out.println("CUPCAKES");
+            System.out.println("Here is the menu...");
+            System.out.println("CUPCAKES: ");
             int itemNumber = 0;
-            for (int i = 0; i < cupcakeMenu.size(); i++) {
-                itemNumber += 1;
+            for (int i = 0;i < cupcakeMenu.size(); i++) {
+                itemNumber++;
                 System.out.println(itemNumber);
                 cupcakeMenu.get(i).type();
-                System.out.println("Price: " + cupcakeMenu.get(i).price);
-                System.out.println("\n");
+                System.out.println("Price: " + cupcakeMenu.get(i).getPrice());
+                System.out.println();
+                Thread.sleep(800);
             }
-            System.out.println("Drinks");
-
+            System.out.println("DRINKS:");
             for (int i = 0; i < drinkMenu.size(); i++) {
-                itemNumber += 1;
+                itemNumber++;
                 System.out.println(itemNumber);
                 drinkMenu.get(i).type();
-                System.out.println("Price: " + drinkMenu.get(i).price);
-                System.out.println("\n");
+                System.out.println("Price: " + drinkMenu.get(i).getPrice());
+                System.out.println();
+                Thread.sleep(800);
             }
-
             boolean ordering = true;
             while (ordering){
                 System.out.println("What would you like to order? Please use the number associated with each item to order.");
-
                 int orderChoice = input.nextInt();
                 input.nextLine();
-
-                if (orderChoice == 1) {
+                if (orderChoice == 1){
                     order.add(cupcakeMenu.get(0));
-                    System.out.println("Item added to order");
-                } else if (orderChoice == 2) {
-                    order.add(cupcakeMenu.get(1));
-                    System.out.println("Item added to order");
-                } else if (orderChoice == 3) {
-                    order.add(cupcakeMenu.get(2));
-                    System.out.println("Item added to order");
-                } else if (orderChoice == 4) {
-                    order.add(drinkMenu.get(0));
-                    System.out.println("Item added to order");
-                } else if (orderChoice == 5) {
-                    order.add(drinkMenu.get(1));
-                    System.out.println("Item added to order");
-                } else if (orderChoice == 6) {
-                    order.add(drinkMenu.get(2));
-                    System.out.println("Item added to order");
-                } else {
-                    System.out.println("Sorry we don't have that on the menu.");
+                    System.out.println("Item " + orderChoice + " added to order.");
                 }
-
-                System.out.println("Would you like to continue ordering? (Y/N)");
+                else if (orderChoice == 2){
+                    order.add(cupcakeMenu.get(1));
+                    System.out.println("Item " + orderChoice + " added to order.");
+                }
+                else if (orderChoice == 3){
+                    order.add(cupcakeMenu.get(2));
+                    System.out.println("Item " + orderChoice + " added to order.");
+                }
+                else if (orderChoice == 4){
+                    order.add(drinkMenu.get(0));
+                    System.out.println("Item " + orderChoice + " added to order.");
+                }
+                else if (orderChoice == 5){
+                    order.add(drinkMenu.get(1));
+                    System.out.println("Item " + orderChoice + " added to order.");
+                }
+                else if (orderChoice == 6){
+                    order.add(drinkMenu.get(2));
+                    System.out.println("Item " + orderChoice + " added to order.");
+                }
+                else {
+                    System.out.println("Sorry, we don't seem to have that on the menu.");
+                }
+                System.out.println("Would you like to continue ordering? (Y/N");
                 placeOrder = input.nextLine();
-                if(!placeOrder.equalsIgnoreCase("Y")) {
+                if (!placeOrder.equalsIgnoreCase("Y")){
                     ordering = false;
                 }
             }
             System.out.println(order.get(0));
             System.out.println(order.get(1));
-            double subtotal = 0.0;
-            for (int i = 2; i<order.size(); i++){
-                if(order.get(i) == cupcakeMenu.get(0)){
+            double subtotal = 0.00;
+            for (int i = 2; i < order.size(); i++){
+                if (order.get(i).equals(cupcakeMenu.get(0))){
                     cupcakeMenu.get(0).type();
                     System.out.println(cupcakeMenu.get(0).getPrice());
                     subtotal += cupcakeMenu.get(0).getPrice();
-                } else if(order.get(i) == cupcakeMenu.get(1)){
+                }
+                else if (order.get(i).equals(cupcakeMenu.get(1))){
                     cupcakeMenu.get(1).type();
                     System.out.println(cupcakeMenu.get(1).getPrice());
                     subtotal += cupcakeMenu.get(1).getPrice();
-                } else if(order.get(i) == cupcakeMenu.get(2)){
+                }
+                else if (order.get(i).equals(cupcakeMenu.get(2))){
                     cupcakeMenu.get(2).type();
                     System.out.println(cupcakeMenu.get(2).getPrice());
                     subtotal += cupcakeMenu.get(2).getPrice();
-                } else if(order.get(i) == drinkMenu.get(0)){
+                }
+                else if (order.get(i).equals(drinkMenu.get(0))){
                     drinkMenu.get(0).type();
                     System.out.println(drinkMenu.get(0).getPrice());
                     subtotal += drinkMenu.get(0).getPrice();
-                } else if(order.get(i) == drinkMenu.get(1)){
+                }
+                else if (order.get(i).equals(drinkMenu.get(1))){
                     drinkMenu.get(1).type();
                     System.out.println(drinkMenu.get(1).getPrice());
-                    subtotal += drinkMenu.get(0).getPrice();
-                } else if(order.get(i) == drinkMenu.get(2)){
+                    subtotal += drinkMenu.get(1).getPrice();
+                }
+                else if (order.get(i).equals(drinkMenu.get(2))){
                     drinkMenu.get(2).type();
                     System.out.println(drinkMenu.get(2).getPrice());
                     subtotal += drinkMenu.get(2).getPrice();
                 }
-                System.out.println("subtotal: " + subtotal);
+                System.out.println("Subtotal: " + subtotal);
+                new CreateFile();
+                new WriteToFile(order);
             }
-            new CreateFile();
-            new WriteToFile(order);
 
-
-        } else {
-            System.out.println("Have a nice day then");
+        }else {
+            System.out.println("Have a nice day then.");
         }
 
     }
-    public static class CreateFile{
-        public CreateFile() {
-            try {
-                File salesData = new File("salesData.txt");
-                if (salesData.createNewFile()) {
-                    System.out.println("File created: " + salesData.getName());
-                } else {
-                    System.out.println("File already exists");
-                }
-            } catch (IOException e) {
-                System.out.println("An error occurred");
+}
+class CreateFile{
+    public CreateFile() throws IOException {
+        try {
+            File salesData = new File("salesData.txt");
+            if (salesData.createNewFile()){
+                System.out.println("File created: " + salesData.getName());
+            }else {
+                System.out.println("File already exists.");
             }
-
+        }catch (IOException e){
+            System.out.println("An error occurred.");
         }
+
     }
-    public static class WriteToFile{
-        public WriteToFile(ArrayList<Object> order){
-            try {
-
-                FileWriter fw = new FileWriter("salesData.txt", true);
-                PrintWriter salesWriter = new PrintWriter(fw);
-                for (int i = 0; i< order.size(); i++){
-                    salesWriter.println(order.get(i));
-                }
-                salesWriter.close();
-                System.out.println("Successfully wrote to the file");
-            } catch (IOException e) {
-                System.out.println("An error occurred");
+}
+class WriteToFile{
+    public WriteToFile(ArrayList<Object> order) throws IOException{
+        try {
+            FileWriter fw = new FileWriter("salesData.txt", true);
+            PrintWriter salesWriter = new PrintWriter(fw);
+            for (Object item: order){
+                salesWriter.println(item);
+                salesWriter.println();
             }
-
-
+            salesWriter.close();
+            System.out.println("Successfully wrote to the file");
+        }catch (IOException e){
+            System.out.println("An error occurred.");
         }
     }
 }
